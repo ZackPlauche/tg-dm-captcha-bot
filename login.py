af25fd6129fc7c4e36fb1ca5a89101e7608242d1
rg <yarik473@gmail.com>
2022-03-28 02:36:42 +0300

    fix login

--- login.py
+++ login.py
@@ -1,16 +1,15 @@
 from configparser import ConfigParser, ExtendedInterpolation, os
 from pyrogram import Client
+from settings import config
 import shutil
 
-parser = ConfigParser(interpolation=ExtendedInterpolation())
-parser.read(os.path.abspath("config.ini"))
 
 
 def login_session():
     app = Client(
         "captcha_bot",
-        api_id=parser["pyrogram"]['api_id'],
-        api_hash=parser["pyrogram"]['api_hash']
+        api_id=config['api_id'],
+        api_hash=config['api_hash']
     )
     app.start()
     me = app.get_me()
